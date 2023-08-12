b307303ff1896a699bb2d18f62cd05d8bcbc7679
mirza-sync <mirzasyahmi123@gmail.com>
2023-08-12 23:59:19 +0800

    Add save department

--- src/main/java/com/mirza/employeemanager/service/DepartmentService.java
+++ src/main/java/com/mirza/employeemanager/service/DepartmentService.java
@@ -3,6 +3,7 @@ package com.mirza.employeemanager.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.mirza.employeemanager.model.Department;
 import com.mirza.employeemanager.repo.DepartmentRepo;
 
 @Service
@@ -12,5 +13,9 @@ public class DepartmentService {
 	@Autowired
 	public DepartmentService(DepartmentRepo departmentRepo) {
 		this.departmentRepo = departmentRepo;
+	}
+
+	public Department addDepartment(Department department) {
+		return this.departmentRepo.save(department);
 	}
 }

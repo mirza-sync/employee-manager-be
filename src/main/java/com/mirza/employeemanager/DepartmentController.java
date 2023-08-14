74b7a447993908cc7acc8030c1144fa6a5e783c6
mirza-sync <mirzasyahmi123@gmail.com>
2023-08-14 23:54:55 +0800

    Feat: Add department endpoint

--- src/main/java/com/mirza/employeemanager/DepartmentController.java
+++ src/main/java/com/mirza/employeemanager/DepartmentController.java
@@ -1,8 +1,13 @@
 package com.mirza.employeemanager;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.mirza.employeemanager.model.Department;
 import com.mirza.employeemanager.service.DepartmentService;
 
 @RestController
@@ -12,5 +17,11 @@ public class DepartmentController {
 	
 	public DepartmentController(DepartmentService departmentService) {
 		this.departmentService = departmentService;
+	}
+	
+	@PostMapping("/add")
+	public ResponseEntity<Department> addDepartment(@RequestBody Department department) {
+		Department newDepartment = departmentService.addDepartment(department);
+		return new ResponseEntity<>(newDepartment, HttpStatus.OK);
 	}
 }
